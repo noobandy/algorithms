1846149d393a259fe907b2f1bbf62854c6c14616
noobandy <noobandy1364@gmail.com>
2017-02-20 17:26:07 +0530

    weighted quick union union finad and weighted quick union union find with path compression

---
+++ src/main/java/algorithms/uf/QuickUnionImprovedUFMain.java
@@ -0,0 +1,30 @@
+package algorithms.uf;
+import java.util.*;
+
+public class QuickUnionImprovedUFMain {
+
+	public static void main(String[] args) {
+	
+		Scanner scanner = new Scanner(System.in);
+
+		int n = scanner.nextInt();
+		UF uf = new QuickUnionImprovedUF(n);
+
+		int commands = scanner.nextInt();
+
+		for(int i = 0; i < commands; i++) {
+			uf.union(scanner.nextInt(), scanner.nextInt());
+		}
+
+		int queries = scanner.nextInt();
+
+		for(int i = 0; i < queries; i++) {
+			int object1 = scanner.nextInt();
+			int object2 = scanner.nextInt();
+			boolean connected = uf.connected(object1, object2);
+			System.out.format("%d and % d are %s connected.%n", object1, object2, connected ? "" : "not");
+		}
+		
+	}
+
+}
