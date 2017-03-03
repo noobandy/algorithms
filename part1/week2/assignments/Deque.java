5de94eb7a8107aea6f0b1a4a6f10f10330b49e50
noobandy <noobandy1364@gmail.com>
2017-03-03 17:07:22 +0530

    compile errors and warnings fixed

--- part1/week2/assignments/Deque.java
+++ part1/week2/assignments/Deque.java
@@ -31,7 +31,7 @@ public class Deque<Item> implements Iterable<Item> {
         }
     }
 
-    private validateRemove() {
+    private void validateRemove() {
         if (isEmpty()) {
             throw new NoSuchElementException();
         }
@@ -105,110 +105,110 @@ public class Deque<Item> implements Iterable<Item> {
             tail = tail.prev;
         }
         size--;
+
+        return item;
     }
 
-    return item;
-}
-// return an iterator over items in order from front to end
+    // return an iterator over items in order from front to end
 
-public Iterator<Item> iterator() {
-    return new DequeIterator(head);
-}
-
-private static class DequeIterator<Item> implements Iterator<Item> {
-
-    private Node<Item> curr;
-    public DequeIterator(Node<Item> head) {
-        curr = head;
+    public Iterator<Item> iterator() {
+        return new DequeIterator<Item>(head);
     }
 
-    public boolean hasNext() {
-        return curr != null;
-    }
+    private static class DequeIterator<Item> implements Iterator<Item> {
 
-    public Item next() {
-        if (curr == null) {
-            throw new NoSuchElementException();
-        } else {
-            Item item = curr.item;
-            curr = curr.next;
-            return item;
+        private Node<Item> curr;
+        public DequeIterator(Node<Item> head) {
+            curr = head;
         }
-    }
-    public void remove() {
-        throw new UnsupportedOperationException();
-    }
 
-}
+        public boolean hasNext() {
+            return curr != null;
+        }
 
+        public Item next() {
+            if (curr == null) {
+                throw new NoSuchElementException();
+            } else {
+                Item item = curr.item;
+                curr = curr.next;
+                return item;
+            }
+        }
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
 
-public static void main(String[] args) {
-    Deque<Integer> deque = new Deque<Integer>();
-    System.out.format("deque is %s empty.%n", deque.isEmpty() ? "" : "not");
-    System.out.format("deque size is %d.%n", deque.size());
-    deque.addFirst(2);
-    deque.addFirst(1);
-    System.out.format("deque is %s empty.%n", deque.isEmpty() ? "" : "not");
-    System.out.format("deque size is %d.%n", deque.size());
-
-    for (Integer num : deque) {
-        System.out.print(num);
-    }
-
-    System.out.println();
-
-    deque.addLast(3);
-    deque.addLast(4);
-
-
-    System.out.format("deque is %s empty.%n", deque.isEmpty() ? "" : "not");
-    System.out.format("deque size is %d.%n", deque.size());
-    for (Integer num : deque) {
-        System.out.print(num);
     }
 
-    System.out.println();
 
-    System.out.format("%d%n", deque.removeFirst());
+    public static void main(String[] args) {
+        Deque<Integer> deque = new Deque<Integer>();
+        System.out.format("deque is %s empty.%n", deque.isEmpty() ? "" : "not");
+        System.out.format("deque size is %d.%n", deque.size());
+        deque.addFirst(2);
+        deque.addFirst(1);
+        System.out.format("deque is %s empty.%n", deque.isEmpty() ? "" : "not");
+        System.out.format("deque size is %d.%n", deque.size());
 
-    System.out.format("deque is %s empty.%n", deque.isEmpty() ? "" : "not");
-    System.out.format("deque size is %d.%n", deque.size());
-    for (Integer num : deque) {
-        System.out.print(num);
+        for (Integer num : deque) {
+            System.out.print(num);
+        }
+
+        System.out.println();
+
+        deque.addLast(3);
+        deque.addLast(4);
+
+
+        System.out.format("deque is %s empty.%n", deque.isEmpty() ? "" : "not");
+        System.out.format("deque size is %d.%n", deque.size());
+        for (Integer num : deque) {
+            System.out.print(num);
+        }
+
+        System.out.println();
+
+        System.out.format("%d%n", deque.removeFirst());
+
+        System.out.format("deque is %s empty.%n", deque.isEmpty() ? "" : "not");
+        System.out.format("deque size is %d.%n", deque.size());
+        for (Integer num : deque) {
+            System.out.print(num);
+        }
+
+        System.out.println();
+
+        System.out.format("%d%n", deque.removeLast());
+
+        System.out.format("deque is %s empty.%n", deque.isEmpty() ? "" : "not");
+        System.out.format("deque size is %d.%n", deque.size());
+        for (Integer num : deque) {
+            System.out.print(num);
+        }
+
+        System.out.println();
+
+        System.out.format("%d%n", deque.removeFirst());
+
+        System.out.format("%d%n", deque.removeLast());
+
+        System.out.format("deque is %s empty.%n", deque.isEmpty() ? "" : "not");
+        System.out.format("deque size is %d.%n", deque.size());
+
+        deque.addFirst(2);
+        deque.addFirst(1);
+        deque.addLast(3);
+        deque.addLast(4);
+
+        System.out.format("deque is %s empty.%n", deque.isEmpty() ? "" : "not");
+        System.out.format("deque size is %d.%n", deque.size());
+
+        for (Integer num : deque) {
+            System.out.print(num);
+        }
+
+        System.out.println();
+
     }
-
-    System.out.println();
-
-    System.out.format("%d%n", deque.removeLast());
-
-    System.out.format("deque is %s empty.%n", deque.isEmpty() ? "" : "not");
-    System.out.format("deque size is %d.%n", deque.size());
-    for (Integer num : deque) {
-        System.out.print(num);
-    }
-
-    System.out.println();
-
-    System.out.format("%d%n", deque.removeFirst());
-
-    System.out.format("%d%n", deque.removeLast());
-
-    System.out.format("deque is %s empty.%n", deque.isEmpty() ? "" : "not");
-    System.out.format("deque size is %d.%n", deque.size());
-
-    deque.addFirst(2);
-    deque.addFirst(1);
-    deque.addLast(3);
-    deque.addLast(4);
-
-    System.out.format("deque is %s empty.%n", deque.isEmpty() ? "" : "not");
-    System.out.format("deque size is %d.%n", deque.size());
-
-    for (Integer num : deque) {
-        System.out.print(num);
-    }
-
-    System.out.println();
-
-}
 }
