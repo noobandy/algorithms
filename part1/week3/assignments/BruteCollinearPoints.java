fdfb7e68883525ec4e148d88144a17ff38a3e33f
Anand Mohan <noobandy1364@gmail.com>
2017-03-08 07:15:56 +0530

    initial commit brute collinear points

--- part1/week3/assignments/BruteCollinearPoints.java
+++ part1/week3/assignments/BruteCollinearPoints.java
@@ -1,5 +1,24 @@
 public class BruteCollinearPoints {
-   public BruteCollinearPoints(Point[] points)    // finds all line segments containing 4 points
-   public           int numberOfSegments()        // the number of line segments
-   public LineSegment[] segments()                // the line segments
+
+	// finds all line segments containing 4 points
+	public BruteCollinearPoints(Point[] points) {
+		if(points == null) {
+			throw new NullPointerException();
+		}
+
+		for(int i = 0; i < points.length; i++) {
+			if(points[i] == null) {
+				throw new NullPointerException();
+			}
+		}
+	}
+	// the number of line segments
+	public int numberOfSegments() {
+		return 0;
+	}
+	// the line segments
+	public LineSegment[] segments() {
+		return null;
+	}
+
 }
